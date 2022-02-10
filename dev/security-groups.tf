af66df49c2a2514ca5a403a902788f7de19a9662
liorh <harel.lior@gmail.com>
2022-02-10 15:46:21 +0200

    allow twingate egress

--- dev/security-groups.tf
+++ dev/security-groups.tf
@@ -1,17 +1,14 @@
 
-resource "aws_security_group" "eks_workers_group_default" {
+  resource "aws_security_group" "eks_workers_group_default" {
   name_prefix = "eks_workers_group_default"
   vpc_id      = module.vpc.vpc_id
 
-  # ingress {
-  #   from_port = 22
-  #   to_port   = 22
-  #   protocol  = "tcp"
-
-  #   cidr_blocks = [
-  #     "10.0.0.0/8",
-  #   ]
-  # }
+  egress {
+    from_port = 5432
+    to_port = 5432   
+    protocol         = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "twingate_connector_outbound" {
@@ -21,7 +18,8 @@ resource "aws_security_group" "twingate_connector_outbound" {
   egress {
     from_port = 30000
     to_port = 31000   
-    protocol         = "-1" 
+    protocol         = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -29,6 +27,20 @@ resource "aws_security_group" "twingate_connector_outbound" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  
 }
+resource "aws_security_group_rule" "rds_dev_copy_sg" {
+  
+  type              = "ingress"
+  security_group_id = "sg-0d6e99ce3e5e835ce"
+  from_port = 5432
+  to_port = 5432
+  protocol = "tcp"
+  description = "eks dev access"
+  source_security_group_id = aws_security_group.eks_workers_group_default.id
+}
+
+# output "eks_node_security_group_id" {
+#     value = aws_security_group.eks_workers_group_default.id
+# }
